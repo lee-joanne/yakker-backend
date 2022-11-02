64dcfd3c87ed98bcb1262e9c1a9303e447812668
Joanne <lee.joanne54@gmail.com>
2022-11-02 21:16:44 +0000

    Edit secret key and add to env.py

--- yakker/settings.py
+++ yakker/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*hf6#xqtkm+(w!8o*f@*z2efg&!4i-%di0z6_n6wqmd&js)run'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
