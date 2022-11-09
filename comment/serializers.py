98831f261e3261043472a3c6b5c75e8cf5afc707
Joanne <lee.joanne54@gmail.com>
2022-11-09 19:17:31 +0000

    Create reyakks app

--- comment/serializers.py
+++ comment/serializers.py
@@ -3,8 +3,7 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    commenter = serializers.ReadOnlyField(source='commenter.username')
-    # post = serializers.ReadOnlyField(source='post.title')
+    commenter = serializers.ReadOnlyField(source='commenter.username')``
 
     class Meta:
         model = Comment
