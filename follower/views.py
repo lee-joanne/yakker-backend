890112bea4d48d34a5e9150794ce84a128a51518
Joanne <lee.joanne54@gmail.com>
2022-11-12 17:27:37 +0000

    Create views.py for follower

--- follower/views.py
+++ follower/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Follower
+from yakker.permissions import AuthorOrReadOnly
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class ListFollower(generics.ListCreateAPIView):
+    """
+    Class-based view to list all posts.
+    """
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class DetailFollower(generics.RetrieveDestroyAPIView):
+    """
+    Class-based detailed view to retrieve a post.
+    Can edit, or delete a post if you're the author.
+    """
+    queryset = Follower.objects.all()
+    permission_classes = [AuthorOrReadOnly]
+    serializer_class = FollowerSerializer
