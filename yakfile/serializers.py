d431a3f0d1facfc0004d25b1047e7c0b1482f4de
Joanne <lee.joanne54@gmail.com>
2022-11-15 22:27:44 +0000

    Add get_following_id serializer method to yakfile app serializer.py file

--- yakfile/serializers.py
+++ yakfile/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 from rest_framework import serializers
 from .models import Yakfile
+from follower.models import Follower
 
 
 class YakfileSerializer(serializers.ModelSerializer):
@@ -9,6 +10,7 @@ class YakfileSerializer(serializers.ModelSerializer):
     is_author = serializers.SerializerMethodField()
     author_age_days = serializers.SerializerMethodField()
     post_count = serializers.ReadOnlyField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_author(self, obj):
         request = self.context.get('request', None)
@@ -17,9 +19,22 @@ class YakfileSerializer(serializers.ModelSerializer):
     def get_author_age_days(self, obj):
         return (now() - obj.created_at).days
 
+    def get_following_id(self, obj):
+        """
+        Code on get_following_id taken from CI's DRF Example Project
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            followed_user = Follower.objects.filter(
+                author=user, followed_user=obj.author
+            ).first()
+            print(followed_user)
+            return followed_user.id if followed_user else None
+        return None
+
 
     class Meta:
         model = Yakfile
         fields = [
-            'id', 'author', 'created_at', 'image', 'content', 'is_author', 'author_age_days', 'post_count'
+            'id', 'author', 'created_at', 'image', 'content', 'is_author', 'author_age_days', 'post_count', 'following_id',
         ]
