978010ac93bd079a84c32f3c52abc379921b4dd9
Joanne <lee.joanne54@gmail.com>
2022-11-16 18:37:48 +0000

    Add id to serializers.py file for post_reyakks, fix source from 'liker' to 'post_reyakker'

--- post_reyakks/serializers.py
+++ post_reyakks/serializers.py
@@ -4,12 +4,12 @@ from .models import PostReyakks
 
 
 class PostReyakksSerializer(serializers.ModelSerializer):
-    post_reyakker = serializers.ReadOnlyField(source='liker.username')
+    post_reyakker = serializers.ReadOnlyField(source='post_reyakker.username')
 
     class Meta:
         model = PostReyakks
         fields = [
-            'post_reyakker', 'created_at', 'post'
+            'id', 'post_reyakker', 'created_at', 'post'
         ]
 
     def create(self, validated_data):
